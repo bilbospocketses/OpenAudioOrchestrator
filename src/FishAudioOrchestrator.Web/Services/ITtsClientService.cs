c9b44228e9431acecb53c00bb9095166f13b2334
Jamie Chapman <104535858+bilbospocketses@users.noreply.github.com>
2026-04-02 01:24:23 -0400

    fix: remove dead code GenerateAsync and SaveGenerationLogAsync from TtsClientService (QUAL-13)

--- src/FishAudioOrchestrator.Web/Services/ITtsClientService.cs
+++ src/FishAudioOrchestrator.Web/Services/ITtsClientService.cs
@@ -1,5 +1,3 @@
-using FishAudioOrchestrator.Web.Data.Entities;
-
 namespace FishAudioOrchestrator.Web.Services;
 
 public class TtsRequest
@@ -9,18 +7,7 @@ public class TtsRequest
     public string Format { get; set; } = "wav";
 }
 
-public class TtsResult
-{
-    public required string OutputFileName { get; set; }
-    public required string OutputPath { get; set; }
-    public long DurationMs { get; set; }
-}
-
 public interface ITtsClientService
 {
-    Task<TtsResult> GenerateAsync(string containerBaseUrl, TtsRequest request,
-        int modelProfileId, int? referenceVoiceId);
     Task<bool> GetHealthAsync(string baseUrl);
-    Task<GenerationLog> SaveGenerationLogAsync(int modelProfileId, int? referenceVoiceId,
-        string inputText, string outputFileName, string format, long durationMs);
 }
